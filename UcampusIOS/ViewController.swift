3e0b94767b5ee94a745823f8152c08a31d797b66
soonoo <qpseh2m7@gmail.com>
2018-02-17 23:28:16 +0900

    initial commit

--- UcampusIOS/ViewController.swift
+++ UcampusIOS/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var bgImageView: UIImageView!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // make login button border radius
+        loginButton.layer.cornerRadius = 6
+        loginButton.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.setAnimationRepeatAutoreverses(true)
+        UIView.animate(withDuration: 100, delay: 0, options: [.repeat, .autoreverse], animations: {
+            self.bgImageView.center.x -= (self.bgImageView.image!.size.width - self.view.bounds.width)
+        }, completion: nil)
+    }
+    
+    @IBAction func tryLogin(_ sender: UIButton) {
+        let url = "https://info.kw.ac.kr/webnote/login/login_proc.php"
+        let params: Parameters = [
+            "login_type": "2",
+            "check_svc": "",
+            "redirect_url": "http%3A%2F%2Finfo.kw.ac.kr%2F",
+            "layout_opt": "",
+            "gubun_code": "11",
+            "p_language": "KOREAN",
+            "member_no": "2014722023",
+            "password": "c792b"
+        ]
+        
+        Alamofire.request(url, method: .post, parameters: params, encoding: URLEncoding.httpBody).responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+    }
 }
-
